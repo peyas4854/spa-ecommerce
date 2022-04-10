1cc668734d61155adabbbf5f5670062a213050a0
Peyas  chandra das <peyas3500@gmail.com>
2022-04-11 00:37:56 +0600

    product factory update

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -16,7 +16,7 @@ class ProductFactory extends Factory
         return [
             'name'        => $this->faker->text(12),
             'price'       => $this->faker->randomFloat(3, 10, 100),
-            'image'       => "{$this->faker->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])}.jpg",
+            'image'       => "{$this->faker->randomElement([1, 2, 3])}.jpeg",
             'description' => $this->faker->text(300),
             'created_by'  => 1,
         ];
